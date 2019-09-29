0af8a24885082bc629d464e795d0ae9b04d768ba
the6th <hayashi.tomoki@outlook.com>
2019-09-29 14:16:51 +0900

    save builded sln it success to build ARM-windows

---
+++ cmake/build/x64/third_party/zlib/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/GitHub/grpc-the6th/third_party/zlib
+# Build directory: C:/GitHub/grpc-the6th/cmake/build/x64/third_party/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "C:/GitHub/grpc-the6th/third_party/zlib/CMakeLists.txt;235;add_test;C:/GitHub/grpc-the6th/third_party/zlib/CMakeLists.txt;0;")
